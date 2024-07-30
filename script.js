75fe7e206e998ba935817131e6e2e38c3424eeed
A2gamer2345V <A2gamer2345v@gmail.com>
2024-07-30 19:37:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,3 @@ function moveRandomEl(elm) {
   moveRandom.addEventListener("mouseenter", function (e) {
     moveRandomEl(e.target);
   });
-
-function sendEmail(action, url) {
-    console.log(`Sending email with action: ${action}`); // Debug log
-
-    emailjs.send('service_s1h6nmn', 'template_9jmhf7n', {
-        action: action
-    }).then((response) => {
-        console.log('Success:', response);
-        window.location.href = url; // Navigate to the URL after sending the email
-    }).catch((error) => {
-        console.error('Error:', error);
-        alert('Failed to send email. Please try again later.'); // Notify user of failure
-    });
-}
